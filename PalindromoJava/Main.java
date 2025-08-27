971bff6b47c97f8a84fd32a371a0c457699839d4
Rafael Assis Carvalho Lacerda <rafaelassisclacerda@gmail.com>
2025-08-26 21:02:20 -0300

    Add files via upload

--- PalindromoJava/Main.java
+++ PalindromoJava/Main.java
@@ -2,6 +2,27 @@ import java.util.Scanner;
 
 
 public class Main {
+
+	public static boolean compareManual(String str1, String str2) {
+		boolean resposta = true;
+
+		if (str1.length() != str2.length()) {
+            		resposta = false;
+        	}else{
+			boolean diferente = false;
+			int i = 0;
+        		while (i < str1.length() && !diferente) {
+            			if (str1.charAt(i) != str2.charAt(i)) {
+                			diferente = true;
+					resposta = false;
+            			}
+			i++;
+        		}
+		}
+        	return resposta;
+    	}
+
+
 	public static boolean ehPalindromo(String str) {
 
         	int i = 0;
@@ -20,19 +41,24 @@ public class Main {
 	public static void main(String[] args) {
 	        Scanner scanner = new Scanner(System.in);
 
-	        while (true) {
+		String flag = "FIM"; //string de termino
+		boolean continuar = true; //flag booleana
+
+
+	        while (continuar) { //loop para continuar até encontrar o FIM
 	            String linha = scanner.nextLine();
 
-	            if (linha.equals("FIM")) {
-	                break;
+	            if (compare(linha, flag)) { //verifica ocorrencia do FIM e decide se o >
+            		continuar = false;
+        	    }else{
+                	if (ehPalindromo(linha)) { //if e else que verifica se é palind>
+                        	System.out.println("SIM");
+                	} else {
+                        	System.out.println("NAO");
+                	}
 	            }
-
-	            if (ehPalindromo(linha)) {
-	                System.out.println("SIM");
-	            } else {
-	                System.out.println("NAO");
-	            }
-	        }
+		
+	         }
         scanner.close();
-    }
+	}
 }
