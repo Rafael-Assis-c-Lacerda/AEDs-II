884e9e123dfd0104b3f6e655c53c4fc752a573ad
Rafael Assis Carvalho Lacerda <rafaelassisclacerda@gmail.com>
2025-09-25 15:09:11 -0300

    Add files via upload

--- lab5/Main.java
+++ lab5/Main.java
@@ -1,6 +1,6 @@
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
@@ -46,8 +46,8 @@ public class Main {
                 j--;
             }
         }
-        if (esq < j)  quicksort(array, esq, j);
-        if (i < dir)  quicksort(array, i, dir);
+        if (esq < j)  quicksortFirst(array, esq, j);
+        if (i < dir)  quicksortFirst(array, i, dir);
     }
 
     public static void quicksortLast(int [] array,int esq, int dir){
@@ -62,13 +62,12 @@ public class Main {
                 j--;
             }
         }
-        if (esq < j)  quicksort(array, esq, j);
-        if (i < dir)  quicksort(array, i, dir);
+        if (esq < j)  quicksortLast(array, esq, j);
+        if (i < dir)  quicksortLast(array, i, dir);
     }
 
-    public static int median(int [] array,int esq, int dir){
+    public static int median(int[] array, int esq, int dir) {
         int mid = (esq + dir) / 2;
-    
         int a = array[esq];
         int b = array[mid];
         int c = array[dir];
@@ -80,24 +79,40 @@ public class Main {
         } else {
             if (b < c) return mid;
             else if (a > c) return esq;
-            else return dir; 
+            else return dir;
         }
     }
 
-    public static void quicksortMedian(int [] array,int esq, int dir){
-        int i = esq, j = dir;
-        int pivo = array[median(array, esq, dir)];
+    public static void quicksortMedian(int[] array, int esq, int dir) {
+        if (esq >= dir) return;
+
+        // Escolhe pivô pela mediana de três e move para o início
+        int med = median(array, esq, dir);
+        swap(array, esq, med); // move a mediana para o início
+        int pivo = array[esq];
+
+        int i = esq + 1;  // Começa de esq + 1, já que o pivô está na posição esq
+        int j = dir;
+
         while (i <= j) {
-            while (array[i] < pivo) i++;
-            while (array[j] > pivo) j--;
+            // Encontre um elemento maior que o pivô
+            while (i <= dir && array[i] < pivo) i++;
+            // Encontre um elemento menor que o pivô
+            while (j >= esq && array[j] > pivo) j--;
+
             if (i <= j) {
                 swap(array, i, j);
                 i++;
                 j--;
             }
         }
-        if (esq < j)  quicksort(array, esq, j);
-        if (i < dir)  quicksort(array, i, dir);
+
+        // Coloca o pivô na posição correta
+        swap(array, esq, j);
+
+        // Recursão nas duas metades
+        quicksortMedian(array, esq, j - 1);
+        quicksortMedian(array, j + 1, dir);
     }
 
     // Gera arrays para teste
@@ -134,62 +149,48 @@ public class Main {
     public static int[] cloneArray(int[] original) {
         return Arrays.copyOf(original, original.length);
     }
-    public static void main (String[]args){
-        //criação do array e escolha do tamanho
+    public static void main(String[] args) {
+        // Criação do array e escolha do tamanho
         int[] array;
-        int size = 1000;
+        int size = 10000;
 
-        //gera o array original
-
-        array = generateRandomArray(size);
+        // Gera o array original
+        //array = generateRandomArray(size);
         //array = generateSortedArray(size);
-        //array = generateAlmostSortedArray(size);
+        array = generateAlmostSortedArray(size);
 
-
-        //faz a ordenação com o quicksort com pivo aleatorio
+        // Quicksort com pivô aleatório
         long start = System.nanoTime();
-
-        int[] copyForRandomPivot = cloneArray(array);
-        quicksort(copyForRandomPivot, 0, copyForRandomPivot.length - 1);
-
+        int[] copyForRandomQuicksort = cloneArray(array);
+        quicksort(copyForRandomQuicksort, 0, copyForRandomQuicksort.length - 1);
         long end = System.nanoTime();
-        long duration = end - start;  // duração em nanossegundos
-        System.out.println("Quicksort aleatorio:" + size);
-        System.out.println("Tempo decorrido: " + duration / 1_000_000.0 + " ms");
+        long duration = end - start;
+        System.out.println("Tempo para Quicksort Aleatório com tamanho de array " + size + ": " + duration / 1_000_000.0 + " ms");
 
-
+        // Quicksort com pivô no início
         start = System.nanoTime();
-
-        //faz a ordenação com o quicksort com pivo aleatorio
-        start = System.nanoTime();
-
-        int[] copyForRandomPivot = cloneArray(array);
-        quicksort(copyForRandomPivot, 0, copyForRandomPivot.length - 1);
-
+        int[] copyForStartQuicksort = cloneArray(array);
+        quicksortFirst(copyForStartQuicksort, 0, copyForStartQuicksort.length - 1);
         end = System.nanoTime();
-        duration = end - start;  // duração em nanossegundos
-        System.out.println("Quicksort aleatorio:" + size);
-        System.out.println("Tempo decorrido: " + duration / 1_000_000.0 + " ms");
+        duration = end - start;
+        System.out.println("Tempo para Quicksort com Pivô no Início com tamanho de array " + size + ": " + duration / 1_000_000.0 + " ms");
 
-
+        // Quicksort com pivô no final
         start = System.nanoTime();
-
-        //faz a ordenação com o quicksort com pivo aleatorio
-        start = System.nanoTime();
-
-        int[] copyForRandomPivot = cloneArray(array);
-        quicksort(copyForRandomPivot, 0, copyForRandomPivot.length - 1);
-
+        int[] copyForEndQuicksort = cloneArray(array);
+        quicksortLast(copyForEndQuicksort, 0, copyForEndQuicksort.length - 1);
         end = System.nanoTime();
-        duration = end - start;  // duração em nanossegundos
-        System.out.println("Quicksort aleatorio:" + size);
-        System.out.println("Tempo decorrido: " + duration / 1_000_000.0 + " ms");
+        duration = end - start;
+        System.out.println("Tempo para Quicksort com Pivô no Final com tamanho de array " + size + ": " + duration / 1_000_000.0 + " ms");
 
-
+        // Quicksort com pivô mediana de três
         start = System.nanoTime();
-
-        
+        int[] copyForMedianQuicksort = cloneArray(array);
+        quicksortMedian(copyForMedianQuicksort, 0, copyForMedianQuicksort.length - 1);
+        end = System.nanoTime();
+        duration = end - start;
+        System.out.println("Tempo para Quicksort com Mediana de Três com tamanho de array " + size + ": " + duration / 1_000_000.0 + " ms");
 
         scanner.close();
-    }    
+    }
 }
