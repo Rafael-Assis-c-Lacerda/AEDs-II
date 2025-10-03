02e6d6ffe56d29bc4ed62b3ab6df3c0fe78f9759
Rafael Assis Carvalho Lacerda <rafaelassisclacerda@gmail.com>
2025-10-03 02:48:13 -0300

    Add files via upload

--- tp4/Main.java
+++ tp4/Main.java
@@ -4,7 +4,7 @@ import java.io.*;
 class Game{
     private int id;
     private String name;
-    private String Date;
+    private String date;
     private int jogadores;
     private float preco;
     private String [] linguas;
@@ -17,20 +17,244 @@ class Game{
     private String [] generos;
     private String [] tags;
 
-    public void setID(String id){
+
+    //metodos sets (utilizados para popular o objeto)
+    public void setId(String id){
         this.id = Integer.parseInt(id);
     }
     public void setNome(String name) {
         this.name = name;
     }
-    public int getID(){
+    public void setData(String data) {
+        String mes = "", dia = "", ano = "", resp = "";
+        for(int i = 0; i < 3; i++) {
+            mes += data.charAt(i);
+        }
+        if(data.charAt(5) == ',') {
+            dia += "0";
+            dia += data.charAt(4);
+            for(int i = 7; i < data.length(); i++) {
+                ano += data.charAt(i);
+            }
+        }
+        else {
+            dia += data.charAt(4);
+            dia += data.charAt(5);
+            for(int i = 8; i < data.length(); i++) {
+                ano += data.charAt(i);
+            }
+        }
+        switch(mes) {
+            case "Jan": mes = "01"; break;
+            case "Feb": mes = "02"; break;
+            case "Mar": mes = "03"; break;
+            case "Apr": mes = "04"; break;
+            case "May": mes = "05"; break;
+            case "Jun": mes = "06"; break;
+            case "Jul": mes = "07"; break;
+            case "Aug": mes = "08"; break;
+            case "Sep": mes = "09"; break;
+            case "Oct": mes = "10"; break;
+            case "Nov": mes = "11"; break;
+            case "Dec": mes = "12"; break;
+            default: mes = "01"; break;     
+        }
+        resp = dia + "/" + mes + "/" + ano;
+        this.date = resp;
+    }
+    public void setJogadores(String jogadores) {
+        String aux = "";
+        for(int i = 0; i < jogadores.length(); i++) {
+            if(jogadores.charAt(i) >= '0' && jogadores.charAt(i) <= '9') {
+                aux += jogadores.charAt(i);
+            }
+        }
+        this.jogadores = Integer.parseInt(aux);
+    }
+
+    public void setPreco(String preco) {
+        if(compare(preco,"Free to play")) {
+            this.preco = 0.0f;
+        }
+        else {
+            this.preco = Float.parseFloat(preco);
+        } 
+    }
+
+    public void setLinguas(String linguas) {
+        this.linguas = formatar(linguas);
+    }
+
+    public void setNotaEspecial(String notaEspecial) {
+        if(notaEspecial == null) {
+            this.notaEspecial = -1;
+        }
+        else {
+            this.notaEspecial = Integer.parseInt(notaEspecial);
+        }
+    }
+
+    public void setNotaUsuario(String notaUsuario) {
+        if(notaUsuario == null || notaUsuario == "tbd") {
+            this.notaUsuario = -1.0f;
+        }
+        else {
+            this.notaUsuario = Float.parseFloat(notaUsuario);
+        }
+    }
+
+    public void setConquistas(String conquistas) {
+        if(conquistas == null) {
+            this.conquistas = 0;
+        }
+        else {
+            this.conquistas = Integer.parseInt(conquistas);
+        } 
+    }
+
+    public void setPublishers(String publishers) {
+        this.publishers = formatar(publishers);
+    }
+
+    public void setDevelopers(String developers) {
+        this.developers = formatar(developers);
+    }
+
+    public void setCategorias(String categorias) {
+        this.categorias = formatar(categorias);
+    }
+
+    public void setGeneros(String generos) {
+        this.generos = formatar(generos);
+    }
+
+    public void setTags(String tags) {
+        this.tags = formatar(tags);
+    }
+
+    //metodos get
+    public int getId() {
         return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public int getJogadores() {
+        return this.jogadores;
+    }
+
+    public float getPreco() {
+        return this.preco;
+    }
+
+    public String[] getLinguas() {
+        return this.linguas;
+    }
+
+    public int getNotaEspecial() {
+        return this.notaEspecial;
+    }
+
+    public float getNotaUsuario() {
+        return this.notaUsuario;
+    }
+
+    public int getConquistas() {
+        return this.conquistas;
+    }
+
+    public String[] getPublishers() {
+        return this.publishers;
+    }
+
+    public String[] getDevelopers() {
+        return this.developers;
+    }
+
+    public String[] getCategorias() {
+        return this.categorias;
+    }
+
+    public String[] getGeneros() {
+        return this.generos;
+    }
+
+    public String[] getTags() {
+        return this.tags;
+    }
+
+    //funções auxiliares
+    public static boolean compare(String str1, String str2) { //função que compara duas strings
+		boolean resposta = true;
+
+		if (str1.length() != str2.length()) { //verifica o tamnho, dependendo ja da falso
+            		resposta = false;
+        	}else{
+			boolean diferente = false;
+			int i = 0;
+        		while (i < str1.length() && !diferente) { //while que passa por todos os caracteres
+            			if (str1.charAt(i) != str2.charAt(i)) { //verifica se letra por letra é igual
+                			diferente = true;
+					resposta = false;
+            			}
+			i++;
+        		}
+		}
+        return resposta;
+    }
+    public String printElementosMultiplos(String [] elementos){
+        String aux = "[";
+
+        for(int i = 0; i<elementos.length;i++){
+            aux = aux + elementos[i];
+            if(i<elementos.length - 1){
+                aux = aux + ", ";
+            }
+        }
+        aux = aux + "]";
+        return aux;
+    }
+
+    public String[] formatar(String entrada) { //função utilizada nos casos que tem mais de 1 elemento em uma parte
+        int virgulas = 0;
+        for(int i = 0; i < entrada.length(); i++) {
+            char c = entrada.charAt(i);
+            if(c == ',') {
+                virgulas++;
+            }
+        }
+        String resp[] = new String[virgulas + 1];
+        String aux = "";
+        int contador = 0;
+        for(int i = 0; i < entrada.length(); i++) {
+            char c = entrada.charAt(i); 
+            if(c == ',') {
+                resp[contador] = aux;
+                contador++;
+                aux = "";
+            }
+            else {
+                if(!(c == '[' || c == ']' || c == '\'')) {
+                    aux += c;
+                } 
+            }
+        }
+        resp[contador] = aux;
+        return resp;
+    }
+
+    public String printResultado() {
+        return ("=> " + id + " ## " + name + " ## " + date + " ## " + jogadores + " ## " + preco + " ## " + printElementosMultiplos(linguas) + " ## " + notaEspecial + " ## " + notaUsuario + " ## " + conquistas + " ## " + printElementosMultiplos(publishers) + " ## " + printElementosMultiplos(developers) + " ## " + printElementosMultiplos(categorias) + " ## " + printElementosMultiplos(generos) + " ## " + printElementosMultiplos(tags) + " ## ");
     }
 }
 
 public class Main{
-    static Scanner scanner = new Scanner(System.in);
-    static Scanner scannerArq = new Scanner("games.csv");
 
     public static boolean compare(String str1, String str2) { //função que compara duas strings
 		boolean resposta = true;
@@ -52,17 +276,41 @@ public class Main{
     }
 
     public static void sets(Game game, String array[]){
-
+        game.setId(array[0]);
+        game.setNome(array[1]);
+        game.setData(array[2]);
+        game.setJogadores(array[3]);
+        game.setPreco(array[4]);
+        game.setLinguas(array[5]);
+        game.setNotaEspecial(array[6]);
+        game.setNotaUsuario(array[7]);
+        game.setConquistas(array[8]);
+        game.setPublishers(array[9]);
+        game.setDevelopers(array[10]);
+        game.setCategorias(array[11]);
+        game.setGeneros(array[12]);
+        game.setTags(array[13]);
     }
 
     public static void main(String []args) throws FileNotFoundException{
+        Scanner scanner = new Scanner(System.in);
+        File arq = new File("games.csv");
+        Scanner scannerArq = new Scanner(arq);
+
         Game game[] = new Game[2000];
         int jogos = 0;
 
-        while(scannerArq.hasNextLine()) {
+        //System.out.println("teste1");
+
+        // Pular o cabeçalho
+        if (scannerArq.hasNextLine()) {
             scannerArq.nextLine();
+        }
+
+        while(scannerArq.hasNextLine()) {
 
             String entrada = scannerArq.nextLine();
+
             String array[] = new String[14]; 
             String aux = "";
             int contador = 0;
@@ -72,7 +320,7 @@ public class Main{
                 char c = entrada.charAt(i);
 
                 if(c == '"'){
-                    aspas = true;
+                    aspas = !aspas;
                 
                 }else if(c == ',' && !aspas){
                     array[contador] = aux;
@@ -82,12 +330,16 @@ public class Main{
                     aux = aux + c;
                 }
             }
+
             array[contador] = aux;
+
             game[jogos] = new Game();
             sets(game[jogos], array);
             jogos++;
+            //System.out.println("teste3");
         }
 
+        //System.out.println("teste");
         String flag = "FIM"; //string de termino
 		boolean continuar = true; //flag booleana
         
@@ -97,10 +349,10 @@ public class Main{
 	        if (compare(busca, flag)) { //verifica ocorrencia do FIM e decide se o programa acaba
             	continuar = false;
         	}else{
-                int idBusca = Integer.parseInt(busca);
+                int Busca = Integer.parseInt(busca);
                 for(int i = 0; i < jogos; i++) {
-                    if(idBusca == game[i].getID()) {
-                        System.out.println(game[i].toString());
+                    if(Busca == game[i].getId()) {
+                        System.out.println(game[i].printResultado());
                         i = jogos;
                     }
                 }
